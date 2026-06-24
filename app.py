c2e367bbc5ae8c1c4dcbf11ad31d390902ae824b
user <Kodra.Abela-25@stud.dhbw-ravensburg.de>
2026-06-24 14:34:37 +0200

    Added authentication system

--- app.py
+++ app.py
@@ -1,18 +1,101 @@
-from flask import Flask
-from models import db
+from flask import Flask, render_template, request, redirect, session
+from flask_bcrypt import Bcrypt
+from models import db, User
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "super-secret-key"
+app.config["SECRET_KEY"] = "securevault-secret-key"
 
-app.config["SQLALCHEMY_DATABASE_URI"] = \
-    "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 db.init_app(app)
 
+bcrypt = Bcrypt(app)
+
+
 @app.route("/")
 def home():
-    return "SecureVault läuft erfolgreich!"
+    return redirect("/login")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        existing_user = User.query.filter_by(
+            username=username
+        ).first()
+
+        if existing_user:
+            return "Benutzer existiert bereits"
+
+        password_hash = bcrypt.generate_password_hash(
+            password
+        ).decode("utf-8")
+
+        new_user = User(
+            username=username,
+            password_hash=password_hash
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect("/login")
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(
+            username=username
+        ).first()
+
+        if user and bcrypt.check_password_hash(
+            user.password_hash,
+            password
+        ):
+
+            session["user_id"] = user.id
+            session["username"] = user.username
+
+            return redirect("/dashboard")
+
+        return "Falsche Anmeldedaten"
+
+    return render_template("login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "user_id" not in session:
+        return redirect("/login")
+
+    return render_template(
+        "dashboard.html",
+        username=session["username"]
+    )
+
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect("/login")
+
 
 with app.app_context():
     db.create_all()
